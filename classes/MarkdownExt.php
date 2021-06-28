22a37448fa14ed5f02a19192b02829061614714b
cpsdqs <cpsdqs@gmail.com>
2021-06-28 18:27:12 +0200

    Add tabelo

--- classes/MarkdownExt.php
+++ classes/MarkdownExt.php
@@ -1002,6 +1002,72 @@ class MarkdownExt {
             }
         };
 
+
+        $markdown->addBlockType('[', 'AksoTable', true, true);
+        $markdown->blockAksoTable = function($line, $block) {
+            if (preg_match('/^\[\[tabelo\]\]/', $line['text'], $matches)) {
+                return array(
+                    'char' => $line['text'][0],
+                    'element' => array(
+                        'name' => 'table',
+                        'attributes' => array(
+                            'class' => 'content-mdx-table',
+                        ),
+                        'handler' => 'elements',
+                        'text' => [[]],
+                    ),
+                );
+            }
+        };
+        $markdown->blockAksoTableContinue = function($line, $block) {
+            if (isset($block['complete'])) {
+                return;
+            }
+            if (isset($block['interrupted'])) {
+                // we don't care about blank lines
+                unset($block['interrupted']);
+            }
+
+            if (preg_match('/^\[\[\/tabelo\]\]$/', $line['text'])) {
+                // end of the block
+                $block['complete'] = true;
+                return $block;
+            }
+
+            $block['element']['text'][] = preg_split('/\|/',
+                preg_replace('/^\s*\|/', '',
+                    preg_replace('/\|\s*$/', '', $line['body'])));
+
+            return $block;
+        };
+        $markdown->blockAksoTableComplete = function($block) use ($self) {
+            $columnCount = 0;
+            foreach ($block['element']['text'] as $row) {
+                $columnCount = max($columnCount, count($row));
+            }
+            $rows = [];
+            foreach ($block['element']['text'] as $row) {
+                $cells = [];
+                foreach ($row as $cell) {
+                    $cells[] = array(
+                        'name' => 'td',
+                        'handler' => 'text',
+                        'text' => $cell,
+                    );
+                }
+                $rows[] = array(
+                    'name' => 'tr',
+                    'handler' => 'elements',
+                    'text' => $cells,
+                );
+            }
+            $block['element']['text'] = [array(
+                'name' => 'tbody',
+                'handler' => 'elements',
+                'text' => $rows,
+            )];
+            return $block;
+        };
     }
 
     public $nonces = array('scripts' => [], 'styles' => []);
